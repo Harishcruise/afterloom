ec73b0678d5e92c123925b2e9fcb10f6cbab71d9
Harishcruise <53746760+Harishcruise@users.noreply.github.com>
2023-11-17 16:19:11 -0500

    Color pallete added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,26 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      'black':'#000000',
+      'white': '#ffffff',
+      'background': {
+        100: '#040404',
+        400: '#8C8C8C',
+        500: '#686868',
+        600: '#464646',
+        700: '#262626',
+      },
+      'grey': {
+        100: '#DADADA',
+        200: '#B3B3B3',
+        300: '#B2B2B2',
+      },
+      'light': {
+        100: '#F8F9FA',
+      },
+    },
   },
   plugins: [],
 }
