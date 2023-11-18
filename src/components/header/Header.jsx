344110b2a64e736d58fe69b88227058db9316827
Harishcruise <53746760+Harishcruise@users.noreply.github.com>
2023-11-18 00:18:54 -0500

    added logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Typography } from '../typography'
 import { Button } from '../buttons'
+import logo from '../../assets/logo/AfterLoom Logo.jpg'
 
 function Header() {
   return (
     <div className='flex flex-1 h-[122px] w-full items-center justify-between'>
-        <Typography type="regular16" color="text-white">Logo</Typography>
+        <img alt='' className='w-[150px] object-contain' src={logo}/>
 
         <div className='px-6 py-4 flex items-center justify-center gap-4 border-white'>
             <div className='hover:cursor-pointer hover:opacity-75'>
--- src/sections/top-section/TopSection.jsx
+++ src/sections/top-section/TopSection.jsx
@@ -4,7 +4,7 @@ import { img1, img2, img3 } from '../../assets/images'
 
 function TopSection() {
   return (
-    <div className='flex flex-1 flex-col w-full py-[52px]'>
+    <div className='flex flex-1 flex-col w-full pt-[70px] pb-[52px]'>
         <div className='flex flex-col gap-3 '>
         <Typography type="bold36" color='text-light-100'>We build bridges through digital Experiences</Typography>
         <Typography type="regular16" color='text-grey-300'>We are a team of creative artists, help businesses to build high quality products.</Typography>
