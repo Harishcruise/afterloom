6d0b0496d4d71073f29b69d1e94b799822627a93
Harishcruise <53746760+Harishcruise@users.noreply.github.com>
2023-11-19 12:29:49 -0500

    Added text reveal animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BackgroundWrapper } from "./components/backgroundWrapper";
+import { Header } from "./components/header";
+import { TopSection } from "./sections/top-section"
 import { TextReveal } from "./components/textSlider";
 
 function App() {
 
   return (
     <BackgroundWrapper>
-       <TextReveal/>
+      <Header/>
+      <TopSection/> 
+      <TextReveal type="light64" color="text-background-600">
+      Embark on a journey towards a transformative digital transformation and a successful mobile or web app with our expert guidance and dedicated team.
+      </TextReveal>
     </BackgroundWrapper>
   );
 }
