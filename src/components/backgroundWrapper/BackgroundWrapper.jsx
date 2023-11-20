16017f010cfa33bc75c70bf379bf3b29df693eaf
Harishcruise <53746760+Harishcruise@users.noreply.github.com>
2023-11-19 23:57:36 -0500

    Added Responsive Header

--- src/components/backgroundWrapper/BackgroundWrapper.jsx
+++ src/components/backgroundWrapper/BackgroundWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function BackgroundWrapper({children}) {
   return (
-    <div className='px-12 py-4 h-full w-full flex flex-1 items-center justify-center flex-col'>
+    <div className='px-12 mobile:px-4 py-4 h-full w-full flex flex-1 items-center justify-center flex-col'>
         {children}
     </div>
   )
--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Typography } from '../typography'
 
-function Button({children,onClick}) {
+function Button({children,onClick,responsive}) {
   return (
     <div onClick={onClick} className='inline-flex items-center justify-center px-6 py-4 bg-gradient-to-b from-[#D9D9D9] to-[#D9D9D9] h-[49px] rounded-[80px] shadow-inner shadow-background-400 border hover:cursor-pointer hover:opacity-90 active:opacity-70'>
-        <Typography color='text-[#040404]' type="regular16">{children}</Typography>
+        <Typography responsive={responsive} color='text-[#040404]' type="regular16">{children}</Typography>
     </div>
   )
 }
